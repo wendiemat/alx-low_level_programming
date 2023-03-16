7176ccfa8948aed217ab5d78f0f548013cc9b05d
wendiemat <wendymathabela@gmail.com>
2023-03-16 15:32:12 -0700

    go

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Program that prints a line with a puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+return (0);
+}

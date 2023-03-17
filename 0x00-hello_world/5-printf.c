518c5ca53bee56cbfe0eb0e2aa0f70d722434e9b
wendiemat <wendymathabela@gmail.com>
2023-03-17 05:26:45 -0700

    3+4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - Program should print a line using a function printf
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

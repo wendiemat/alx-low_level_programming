368170365e8e3da6285cbf30fc4da7f182472d18
wendiemat <wendymathabela@gmail.com>
2023-03-21 12:51:14 -0700

    prints

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _isalpha - shows l if the input is a
+letter. Another case, show 0
+* @c: The character in ASCII code
+* 
+* Return: 1 for greater than 0. 0 is zero -1 is less than zero
+*/
+int _abs(int i)
+{
+if (i >= 0)
+{ 
+return (i);
+}
+else
+{
+return (i * -1);
+}
+}

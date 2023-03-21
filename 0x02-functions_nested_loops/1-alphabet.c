98c88f0d3baef001b9ee0058c53d6260b36984da
wendiemat <wendymathabela@gmail.com>
2023-03-21 10:29:43 -0700

    prints

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+* main - prints _putchar to student
+* 
+* Return: 0 (Success)
+*/
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}

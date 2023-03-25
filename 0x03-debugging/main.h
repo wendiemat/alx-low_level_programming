b710d2e62556f704f09af03cf3805a0eaf8a77bb
wendiemat <wendymathabela@gmail.com>
2023-03-25 14:13:27 -0700

    numbers

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN-H
+#define MAIN-H
+#include <stdio.h>
+void positive_or_negative(inti);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert-day(int month, int day);
+#endif

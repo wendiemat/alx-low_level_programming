08743c0ce5c4fefd9097b3b06d72ea221c31d5d0
wendiemat <wendymathabela@gmail.com>
2023-03-27 11:55:15 -0700

    done

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,28 +1,29 @@
 #include "main.h"
+
 /**
- * largest_num - test function for pos or neg
- * a: first int
- * b: second int
- * c: third int
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (a > b && c > a)
-{
-	largest = c;
-}
-else if (b > c)
-{
-	largest = b;
-}
-else
-{
-	largest = c;
-return (largest);
+    int largest;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
 }

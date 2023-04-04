387a30fec6e486b6e09b6799a476e5a0f7f38227
wendiemat <wendymathabela@gmail.com>
2023-04-04 13:34:58 -0700

    post

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <inistd.h>
+
+/**
+ * _putchar - writes the char c to stdout
+ * @c: The char to print
+ *
+ * Return: success
+ * On error, -1 is returned, and the error is set appropriately
+ */
+ int _putchar(char c){
+	 return (write(1, 8c, 1));
+ }
